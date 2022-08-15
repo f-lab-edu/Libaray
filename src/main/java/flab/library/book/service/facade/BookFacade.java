d4b4652e855bd34d4fc565aa070ccd60c28deb7f
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-15 20:47:47 +0900

    feat: #4 Book Facade 작성 - public Long createBook(BookCreateRequest request)   - Book 생성 Facade

---
+++ src/main/java/flab/library/book/service/facade/BookFacade.java
@@ -0,0 +1,21 @@
+package flab.library.book.service.facade;
+
+import static flab.library.book.dto.BookRequestDto.*;
+
+import flab.library.book.domain.entity.Book;
+import flab.library.book.service.command.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BookFacade {
+  private final BookService bookService;
+
+  public Long createBook(BookCreateRequest request) {
+    Book book = request.toEntity();
+    return bookService.createBook(book);
+  }
+
+
+}
