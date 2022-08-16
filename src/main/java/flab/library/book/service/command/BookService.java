9e020ccb63ad13dcf4074ba13866a150e87a353a
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-16 23:34:27 +0900

    feat: #4 책 제목및 컨텐츠에 대한 데이터 수정 (Service,Facade 작성) - 관련 DTO 선언

--- src/main/java/flab/library/book/service/command/BookService.java
+++ src/main/java/flab/library/book/service/command/BookService.java
@@ -1,9 +1,11 @@
 package flab.library.book.service.command;
 
 import flab.library.book.domain.entity.Book;
+import flab.library.book.dto.BookDto;
 
 public interface BookService {
   Long createBook(Book book);
 
   void deleteById(Long bookId);
+  Book updateBook(Long bookId, BookDto.BookUpdateDto updateDto);
 }
--- src/main/java/flab/library/book/service/facade/BookFacade.java
+++ src/main/java/flab/library/book/service/facade/BookFacade.java
@@ -1,8 +1,10 @@
 package flab.library.book.service.facade;
 
 import static flab.library.book.dto.BookRequestDto.*;
+import static flab.library.book.dto.BookResponseDto.*;
 
 import flab.library.book.domain.entity.Book;
+import flab.library.book.dto.BookDto.BookUpdateDto;
 import flab.library.book.service.command.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -20,5 +22,11 @@ public class BookFacade {
   //Todo: Controller에서 bookId에 대한 검증이 필요함. (require)
   public void deleteBook(Long bookId) {
     bookService.deleteById(bookId);
+  }
+
+  public BookUpdateResponse updateBook(Long bookId, BookUpdateRequest request) {
+    BookUpdateDto updateDto = request.toDto();
+    Book book = bookService.updateBook(bookId, updateDto);
+    return new BookUpdateResponse(book);
   }
 }
