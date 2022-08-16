10c7f95dad6848940ceaad52ecb3b8e95ec9d242
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-16 23:34:27 +0900

    feat: #4 책 제목및 컨텐츠에 대한 데이터 수정 (Service,Facade 작성) - 관련 DTO 선언

--- src/main/java/flab/library/book/dto/BookRequestDto.java
+++ src/main/java/flab/library/book/dto/BookRequestDto.java
@@ -3,7 +3,9 @@ package flab.library.book.dto;
 
 import flab.library.book.domain.entity.Book;
 import flab.library.book.domain.entity.BookCategory;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class BookRequestDto {
   @Data
@@ -21,6 +23,17 @@ public class BookRequestDto {
           .category(this.category)
           .build();
     }
-
   }
+
+  @Data
+  @AllArgsConstructor @NoArgsConstructor
+  public static class BookUpdateRequest {
+    private String title;
+    private String content;
+
+    public BookDto.BookUpdateDto toDto() {
+      return new BookDto.BookUpdateDto(title, content);
+    }
+  }
+
 }
---
+++ src/main/java/flab/library/book/dto/BookResponseDto.java
@@ -0,0 +1,25 @@
+package flab.library.book.dto;
+
+import flab.library.book.domain.entity.Book;
+import flab.library.book.domain.entity.BookCategory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class BookResponseDto {
+
+  @Getter
+  public static class BookUpdateResponse {
+    private String title;
+    private String content;
+    private String isbn;
+    private BookCategory category;
+
+    public BookUpdateResponse(Book book) {
+      this.title = book.getTitle();
+      this.content = book.getContent();
+      this.isbn = book.getIsbn();
+      this.category = book.getCategory();
+    }
+  }
+}
