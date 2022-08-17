1a277efa2a47dc3b3b737a1df2a4067f8379ff67
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-17 13:44:45 +0900

    feat: #4 전체 책 목록 조회 Service및 Facade 작성

--- src/main/java/flab/library/book/service/facade/BookFacade.java
+++ src/main/java/flab/library/book/service/facade/BookFacade.java
@@ -6,13 +6,17 @@ import static flab.library.book.dto.BookResponseDto.*;
 import flab.library.book.domain.entity.Book;
 import flab.library.book.dto.BookDto.BookUpdateDto;
 import flab.library.book.service.command.BookService;
+import flab.library.book.service.query.BookQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class BookFacade {
   private final BookService bookService;
+  private final BookQueryService bookQueryService;
 
   public BookCreateResponse createBook(BookCreateRequest request) {
     Book book = request.toEntity();
@@ -29,5 +33,9 @@ public class BookFacade {
     BookUpdateDto updateDto = request.toDto();
     Book book = bookService.updateBook(bookId, updateDto);
     return new BookUpdateResponse(book);
+  }
+
+  public Page<BookResponse> getAllBooks(Pageable pageable) {
+    return bookQueryService.getBooks(pageable).map(BookResponse::new);
   }
 }
