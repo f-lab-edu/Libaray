0c8dc1049b8f5738b4c387f6832946d2a2ecf7cf
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-21 02:18:19 +0900

    feat: #12 대여(Rental)에 대한 Entity 필드 생성 - 대여 시작일 - 대여 종료일 - 반납 여부

--- src/main/java/flab/library/rental/domain/entity/Rental.java
+++ src/main/java/flab/library/rental/domain/entity/Rental.java
@@ -1,15 +1,30 @@
 package flab.library.rental.domain.entity;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
 
+@Getter
 @Entity
 public class Rental {
+    /**
+     * 독립적인 식별자는 필요 없을지 추후 논의하고 싶은 부분이 있습니다.
+     * 추가적으로 전부 참조 관계로 유지해도 될지도 논의해보고 싶습니다.
+     */
     @Id
     @EmbeddedId
     RentalId rentalId;
 
+    private Boolean isReturn;
 
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime startDate;
 
+    @Column(nullable = false)
+    private LocalDateTime endDate;
 }
