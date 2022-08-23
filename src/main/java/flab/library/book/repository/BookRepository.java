a7ad670879715aed24fd4fb23ad9191c16438cf1
손현호 <icraft2170@gmail.com>
2022-08-23 18:31:06 +0900

    chore: Computer 변경 이슈로 인한 Commit-

--- src/main/java/flab/library/book/repository/BookRepository.java
+++ src/main/java/flab/library/book/repository/BookRepository.java
@@ -2,10 +2,16 @@ package flab.library.book.repository;
 
 import flab.library.book.domain.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.HashMap;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    HashMap<String, Book> findAllByISBN(List<String> isbnList);
+    @Query("select b.isbn, b " +
+            "from Book b " +
+            "where b.isbn in (:isbnList) " +
+            "group by b.isbn ")
+    HashMap<String, Book> findAllByISBN(@Param("isbnList") List<String> isbnList);
 }
