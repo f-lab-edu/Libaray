f7173eddb96f60252ccf462c924539c2c2aa584d
손현호 <icraft2170@gmail.com>
2022-08-23 14:11:44 +0900

    feature: #21 현재 대여중인 대여목록 조회 관련 Controller 구현 - `GET: /v1/rental`

--- src/main/java/flab/library/rental/contorller/CheckoutController.java
+++ src/main/java/flab/library/rental/contorller/CheckoutController.java
@@ -2,16 +2,28 @@ package flab.library.rental.contorller;
 
 import flab.library.common.dto.CommonResponse;
 import flab.library.rental.domain.RentalUsers;
+import flab.library.rental.dto.BookRentalResponseDto;
 import flab.library.rental.service.facade.RentalFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static flab.library.rental.dto.BookRentalResponseDto.*;
 
 @RestController
 @RequestMapping("/v1/rental")
 @RequiredArgsConstructor
 public class CheckoutController {
     private final RentalFacade rentalFacade;
+
+    @GetMapping
+    public CommonResponse<Page<BookRentalInfo>> getOngoingRentals(@PageableDefault(page = 0, size = 10)
+                                                                      Pageable pageable) {
+        return CommonResponse.success(rentalFacade.getOngoingRentalInfo(pageable));
+    }
 
     @GetMapping("/isbn/{isbn}")
     ResponseEntity<CommonResponse<RentalUsers>> getRentalUserList(@PathVariable String isbn){
@@ -26,7 +38,7 @@ public class CheckoutController {
     }
 
     @PutMapping("/{rentalId}/return")
-    CommonResponse<Void> returnBook(@PathVariable Long rentalId) {
+    public CommonResponse<Void> returnBook(@PathVariable Long rentalId) {
         rentalFacade.renew(rentalId);
         return CommonResponse.success();
     }
